280ae7073db401db84b3c9a359626c4b52a56b03
apruszkowska <annamariapruszkowska@gmail.com>
2023-05-18 19:23:13 +0200

    created home page

--- codisign/src/App.tsx
+++ codisign/src/App.tsx
@@ -1,13 +1,16 @@
 import { RoutingLinks } from "./routing/routingLinks";
 import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./HomePage/HomePage";
 
 function App() {
   return (
     <ErrorBoundary>
       <RoutingLinks />
       <Routes>
-        <Route path="/" />
+        <Route path="/">
+          <Route index element={<HomePage />} />
+        </Route>
         <Route path="register">
           <Route index element={<div>login</div>} />
         </Route>
