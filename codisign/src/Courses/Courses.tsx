823e698544b52ffedd88ad06482863ddef1b0110
apruszkowska <annamariapruszkowska@gmail.com>
2023-05-19 17:50:53 +0200

    editing courses page

--- codisign/src/Courses/Courses.tsx
+++ codisign/src/Courses/Courses.tsx
@@ -1,5 +1,6 @@
 import CoursesStyles from "./Courses.module.css";
 import { RoutingCourses } from "../routing/routingCourses";
+import { Link } from "react-router-dom";
 
 export const Courses = () => {
   return (
@@ -18,6 +19,18 @@ export const Courses = () => {
           <div className={CoursesStyles.six}>6</div>
           <div className={CoursesStyles.seven}>7</div>
           <div className={CoursesStyles.eight}>8</div>
+        </div>
+
+        <div className="addNewCourse">
+          <p className={CoursesStyles.addCourseLink}>
+            <Link className={CoursesStyles.addCourseLinkText} to="/addCourse">
+              ADD NEW COURSE
+            </Link>
+
+            <Link className={CoursesStyles.plusCourseLink} to="/addCourse">
+              +
+            </Link>
+          </p>
         </div>
       </section>
     </div>
