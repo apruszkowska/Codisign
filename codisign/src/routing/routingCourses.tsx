04f1c33c7cb0445610a7e836a68b4dcd79d504fb
apruszkowska <annamariapruszkowska@gmail.com>
2023-05-20 15:38:07 +0200

    addCourse styled 1200, 1400, 1600, 2000

--- codisign/src/routing/routingCourses.tsx
+++ codisign/src/routing/routingCourses.tsx
@@ -4,8 +4,8 @@ import RoutingCoursesStyles from "./RoutingCourses.module.css";
 export const RoutingCourses = () => {
   return (
     <header className={RoutingCoursesStyles.headerRoutingCourse}>
-      <nav>
-        <ul>
+      <nav className={RoutingCoursesStyles.navRouting}>
+        <ul className={RoutingCoursesStyles.ulRouting}>
           <li className={RoutingCoursesStyles.logoRoutingCourse}>
             <Link to="/">CODISIGN</Link>
           </li>
